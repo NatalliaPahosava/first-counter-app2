d4b1de24554dd75971582b67a55991036b4aba8f
NatalliaPahosava <mulato4ka114@gmail.com>
2022-06-23 15:43:49 -0400

    with reseat

--- scripts.js
+++ scripts.js
@@ -1,25 +1,31 @@
 const counterEl = document.getElementById('counter')
 const totalCount = document.getElementById('total')
 
-
-let count=0
-function increase(){
-// get element from html
-//    wrong const count= counterEl+1
-//    counterEl.innerText= count
-count++
-counterEl.innerHTML=count
+let count = 0
+function increase() {
+  // get element from html
+  //    wrong const count= counterEl+1
+  //    counterEl.innerText= count
+  console.log('incresase')
+  count++
+  counterEl.innerHTML = count
 }
 
 //function that decreases
-function decrease(){
-   if(count<=0) {counterEl.innerText=0} 
-   else if (count>0){count--
-   counterEl.innerText=count}  
+function decrease () {
+  if (count <= 0) {
+    counterEl.innerText = 0
+  } else if (count > 0) {
+    count--
+    counterEl.innerHTML = count
+  }
 }
-
+function reseat(){
+  count=0
+  counterEl.innerHTML = count
+}
 ///bonus function that prints total
-function printTotal(){
-    let countStr=count+'-'
-    totalCount.innerText+=countStr 
+function printTotal () {
+  let countStr = count + '-'
+  totalCount.innerHTML += countStr
 }
